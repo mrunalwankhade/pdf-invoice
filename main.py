e8fe86e85ba18e100b41d4718913bbbfc871edfd
sunny <mrunal123wankhade@gmail.com>
2023-07-25 03:06:59 +0530

    Added a table data.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import glob
+
+import row as row
 from fpdf import FPDF
 from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
@@ -16,8 +17,30 @@ for filepath in filepaths:
 
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h =8, txt=f"Invoice No.{invoice_no}", ln=1)
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h =8, txt=f"Date: {date}")
+    pdf.cell(w=50, h =8, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    columns_a = df.columns
+    columns_a = [item.replace("_", " ").title() for item in columns_a]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns_a[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns_a[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns_a[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns_a[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns_a[4], border=1, ln=1)
+
+    for index , row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
 
     pdf.output(f"PDFs/{filename}.pdf")
